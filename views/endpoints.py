e2e5c1892a0db48140fe7788c1f3cf0b022336fd
Your Name <usamaahmed772@gmail.com>
2021-04-03 19:07:12 +0500

    modifications in create and delete and get all projects done

--- views/endpoints.py
+++ views/endpoints.py
@@ -24,25 +24,54 @@ class CreateContact(Resource):
                         type=int,
                         required=True,
                         help=BLANK_ERROR_MSG)
-
+    parser.add_argument('email_id',
+                        type=str,
+                        required=True,
+                        help=BLANK_ERROR_MSG)
 
     def post(self):
         data = CreateContact.parser.parse_args()
         user_name = data.get('user_name')
-        first_name=data.get('first_name')
-        last_name=data.get('last_name')
-        phone_number= data.get("phone_number")
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        phone_number = data.get("phone_number")
+        email_id = data.get("email_id")
         try:
-            contact=db.session.query(Contact).filter(Contact.user_name==user_name).first()
+            contact = db.session.query(Contact).filter(Contact.user_name == user_name).first()
+            email = db.session.query(Emails).filter(Emails.email_id == email_id).first()
             if contact:
-                return dict(message="Contact with the user_name already exist")
-            new_contact=Contact(user_name=user_name,first_name=first_name,last_name=last_name,phone_number=phone_number)
+                new_email = Emails(email_id=email_id, contact_id=contact.id)
+                db.session.add(new_email)
+                db.session.commit()
+                return dict(user_name=contact.user_name,first_name=contact.first_name,last_name=contact.last_name,phone_number=
+                            contact.phone_number,email_id=new_email.email_id)
+
+
+            new_contact = Contact(user_name=user_name, first_name=first_name, last_name=last_name,
+                                  phone_number=phone_number)
+
             db.session.add(new_contact)
             db.session.commit()
-            return dict(message="Contact has been Created Successfully",user_name=user_name,first_name=first_name,last_name=last_name,
-                        phone_number=phone_number ),201
+
+            new_email = Emails(email_id=email_id, contact_id=new_contact.id)
+            db.session.add(new_email)
+            db.session.commit()
+            if contact:
+                new_email = Emails(email_id=email_id, contact_id=contact.id)
+                db.session.add(new_email)
+                db.session.commit()
+
+            return dict(message="Contact has been Created Successfully", user_name=user_name, first_name=first_name,
+                        last_name=last_name,
+                        phone_number=phone_number, email_id=email_id), 201
+
+
+
+
         except Exception as error:
             return dict(message=str(error), success=False), 400
+
+
 class DeleteContact(Resource):
     parser = reqparse.RequestParser(bundle_errors=True)
     parser.add_argument('contact_id',
@@ -52,34 +81,29 @@ class DeleteContact(Resource):
 
     def delete(self):
         data = DeleteContact.parser.parse_args()
-        contact_id=data.get("contact_id")
+        contact_id = data.get("contact_id")
         try:
-            db.session.query(Emails).filter(Emails.contact_id ==contact_id).delete(synchronize_session=False)
+            db.session.query(Emails).filter(Emails.contact_id == contact_id).delete(synchronize_session=False)
             db.session.query(Contact).filter(Contact.id == contact_id).delete(synchronize_session=False)
             db.session.commit()
-            return dict(message="The contact is deleted successfully"),201
+            return dict(message="The contact is deleted successfully"), 201
         except Exception as error:
             return dict(message=str(error), success=False), 400
+
+
 class ListAllContacts(Resource):
     def get(self):
         try:
-            contacts=Contact.query.all()
-            contact_list=[]
-            for contact in contacts:
-                contact_list.append({"user_name":contact.user_name,"first_name":contact.first_name,"last_name":contact.last_name,
-                                    "phone_number":contact.phone_number})
-            return dict(all_contacts=contact_list)
+            contacts = Contact.query.all()
+            emails=Emails.query.all()
+
+            contact_list = []
+            emails_list=[]
+            for contact,email in zip(contacts,emails):
+                contact_list.append(
+                    {"user_name": contact.user_name, "first_name": contact.first_name, "last_name": contact.last_name,
+                     "phone_number": contact.phone_number})
+                emails_list.append({"emaild_id":email.email_id,"contact_id":email.contact_id})
+            return dict(all_contacts=contact_list,all_emails=emails_list)
         except Exception as error:
             return dict(message=str(error), success=False), 400
-
-
-
-
-
-
-
-
-
-
-
-
